b238848f5f3719c59293e41d61e38637417a4c14
benjaminsimier <benjaminsimier@gmail.com>
2023-04-22 17:52:34 +0700

    [fix] add antd plugin

---
+++ plugins/antd.ts
@@ -0,0 +1,7 @@
+import { defineNuxtPlugin } from '#app'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(Antd)
+})
